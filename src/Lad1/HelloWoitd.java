114e9dac764f514ae80f41b42593419e6b216f2d
tanaporn <tanaporn.po@rmtsvmail.com>
2020-07-15 14:56:17 +0700

    add more code

--- src/Lad1/HelloWoitd.java
+++ src/Lad1/HelloWoitd.java
@@ -5,7 +5,12 @@ public class HelloWoitd
     public static void main(String[] args) {
 
         System.out.println("Hello Woitd ! ! !");
+        System.out.println("Thanaporn Poonchum");
+        System.out.println("363211760041");
+        System.out.println("คณะเทคโนโลยีการจัดการ");
+        System.out.println("การจัดการเทคโนโลยีสารสนเทศ");
     }
+
 
 
 
