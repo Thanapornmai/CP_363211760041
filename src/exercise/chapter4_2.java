750f729b67f58c66c4e9e89d6aa7947b82671390
Tanaporn03 <tanaporn.po@rmutsvmail.com>
2020-08-05 15:54:41 +0700

    chapter4_1

---
+++ src/exercise/chapter4_2.java
@@ -0,0 +1,20 @@
+package exercise;
+
+import java.util.Scanner;
+
+public class chapter4_2 {
+    public static void main(String[] args) {
+        Scanner Scanner = new Scanner(System.in);
+        System.out.println("Entar integer: ");
+        int a = Scanner.nextInt();
+        System.out.println("Entar real number: ");
+        double b = Scanner.nextDouble();
+        System.out.println("Entar integer: ");
+        int c = Scanner.nextInt();
+        System.out.println("Enter real number: ");
+        double d = Scanner.nextDouble();
+
+        System.out.println(a + " " + b + " " + c + " " + d);
+
+    }
+}
---
+++ src/exercise/chapter4_3.java
@@ -0,0 +1,18 @@
+package exercise;
+
+import java.util.Scanner;
+
+public class chapter4_3 {
+    public static void main(String[] args) {
+        Scanner Scanner = new Scanner(System.in);
+
+        System.out.println("Enter your name: ");
+        String name = Scanner.nextLine();
+
+        System.out.println("Enter your age: ");
+        int x = Scanner.nextInt();
+
+        System.out.println("Enter your email: ");
+        String email = Scanner.nextLine();
+    }
+}
