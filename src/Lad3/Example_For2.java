d183471d29d70ca6ac101aaf181525faa658cdb0
Tanaporn03 <tanaporn.po@rmutsvmail.com>
2020-08-19 15:27:21 +0700

    Example_For2

--- src/Lad3/Example_For2.java
+++ src/Lad3/Example_For2.java
@@ -1,7 +1,22 @@
 package Lad3;
 
+import java.util.Scanner;
+
 public class Example_For2 {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int total = 0;
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Enter Integers: ");
+            int num = sc.nextInt();
+            total += num; // ผลรวมเก็บใน total
+        }
+     //display
+     System.out.println("Total: " + total);
+     System.out.println("Average: " + total/10);
 
 
     }
